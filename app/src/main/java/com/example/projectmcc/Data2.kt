6b510b293e3f2e4bed730d6e8be9506f001d55f3
anonymous-baaka <atharvadhanvate@gmail.com>
2021-10-15 10:23:52 +0530

    third

---
+++ app/src/main/java/com/example/projectmcc/Data2.kt
@@ -0,0 +1,3 @@
+package com.example.projectmcc
+
+data class Data2(var condition: String, var currentTemp: String, var minTemp: String, var maxTemp: String, var feelsLike: String, var pressure: String, var humidity: String, var date: String)
