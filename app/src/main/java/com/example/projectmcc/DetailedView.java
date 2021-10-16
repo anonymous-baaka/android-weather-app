d34aa54a1baa36952d1d11822ed1f6cdedac81a5
anonymous-baaka <atharvadhanvate@gmail.com>
2021-10-16 13:51:37 +0530

    intent2

--- app/src/main/java/com/example/projectmcc/DetailedView.java
+++ app/src/main/java/com/example/projectmcc/DetailedView.java
@@ -33,8 +33,8 @@ public class DetailedView extends AppCompatActivity {
         tv_condition.setText(data.getCondition());
         tv_max.setText(data.getMaxTemp());
         tv_min.setText(data.getMinTemp());
-        tv_pressure.setText(data.getPressure());
-        tv_humidity.setText(data.getHumidity());
+        tv_pressure.setText(data.getPressure()+" hPa");
+        tv_humidity.setText(data.getHumidity()+" %");
         tv_day.setText(parseDate(data.getDate()));
 
         String str_cond=data.getCondition();
